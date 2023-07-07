cac980356de246675c7beaa1f12f0f4128552c42
haruki <haruki@MSI>
2023-07-07 10:34:14 +0900

    線の種類

--- main.cpp
+++ main.cpp
@@ -353,8 +353,8 @@ bool IsCollision(const Segment& line, const Plane& plane) {
 	}
 
 	float t = (plane.distance - Dot(line.origin, plane.normal)) / dot;
-	float length = Length(Normalize(line.deff));
-	if (t >= 0 && t < length) {
+	//float length = Length(Normalize(line.deff));
+	if (t >= 0 && t<=1) {
 		collision = true;
 	}
 
